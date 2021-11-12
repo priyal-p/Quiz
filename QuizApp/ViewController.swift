01639618c15a6140cd997d9bde290fa1581039be
Priyal PORWAL <26priyalmrbd@gmail.com>
2021-11-12 17:09:05 +0530

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Priyal PORWAL on 12/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
