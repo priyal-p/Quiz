48b6650c602b2efa84c2c2c843be1384ba854b42
Priyal PORWAL <26priyalmrbd@gmail.com>
2021-11-16 22:39:55 +0530

    Another way to create question - Hashable Questions

--- QuizApp/NavigationControllerRouter.swift
+++ QuizApp/NavigationControllerRouter.swift
@@ -8,18 +8,18 @@
 import QuizGame
 import UIKit
 
-enum Question {
-    case singleAnswer
-    case multipleAnswer
-}
-
-protocol QuestionType {
-    var type: Question { get }
-}
-
-struct ConcreteQuestion: QuestionType {
-    let question: String
-    let type: Question
+enum Question<T: Hashable> {
+    case singleAnswer(T)
+    case multipleAnswer(T)
+    
+    var hashValue: Int {
+        switch self {
+        case .singleAnswer(let a):
+            return a.hashValue
+        case .multipleAnswer(let b):
+            return b.hashValue
+        }
+    }
 }
 
 protocol ViewControllerFactory {
