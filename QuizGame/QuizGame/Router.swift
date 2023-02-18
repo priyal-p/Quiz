1b73013f6f9a1a0817aaf4096e535a6ccae873dc
Priyal PORWAL <26priyalmrbd@gmail.com>
2023-02-19 03:09:15 +0530

    S02E01 Introduced QuizDelegate to replace Router

--- QuizGame/QuizGame/Router.swift
+++ QuizGame/QuizGame/Router.swift
@@ -10,7 +10,8 @@ public protocol QuizDelegate {
     associatedtype Answer
     typealias AnswerCallback = (Answer) -> Void
     
-    func handle(question: Question, answerCallback: @escaping AnswerCallback)
+    func handle(question: Question,
+                answerCallback: @escaping AnswerCallback)
     func handle(result: Result<Question, Answer>)
 }
 
@@ -19,6 +20,7 @@ public protocol Router {
     associatedtype Question: Hashable
     associatedtype Answer
     typealias AnswerCallback = (Answer) -> Void
-    func routeTo(question: Question, answerCallback: @escaping AnswerCallback)
+    func routeTo(question: Question,
+                 answerCallback: @escaping AnswerCallback)
     func routeTo(result: Result<Question, Answer>)
 }
